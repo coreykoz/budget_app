ab6aa1063d2ef21bfe1c2132972e7dd9d0527ee2
coreykoz <ckozlovski2017@gmail.com>
2020-07-13 22:17:37 -0400

    fixed node

--- server_main.js
+++ server_main.js
@@ -1,13 +1,12 @@
 'use strict';
 
 //1: Import Server classes/DB classes
-const val = require('./Database.js');
-let { Database } = val;
+var Database = require('./Database.js');
 const val2 = require('./Server.js');
 let { Server } = val2;
 
 //2: Create Server/DB objects as constants
-const budget_db = new val('corey-finances');
+const budget_db = new Database('corey-finances');
 const budget_server = new Server(budget_db);
 
 //3: Establish Server
