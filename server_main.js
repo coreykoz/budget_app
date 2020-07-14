4c1f49b78df2105598c7951fbcb952634f23e3c6
coreykoz <ckozlovski2017@gmail.com>
2020-07-13 22:18:46 -0400

    fixed node

--- server_main.js
+++ server_main.js
@@ -6,7 +6,8 @@ const val2 = require('./Server.js');
 let { Server } = val2;
 
 //2: Create Server/DB objects as constants
-const budget_db = new Database('corey-finances');
+//const budget_db = new Database('corey-finances');
+new Database('corey-finances');
 const budget_server = new Server(budget_db);
 
 //3: Establish Server
